f1f74295799dacd5f35a9eb498a18dec8dafb9d7
noman5945 <abdullahnoman.5945@gmail.com>
2026-06-10 10:42:40 +0200

    Initial commit

---
+++ test/valkirye-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ValkiryeServerless from '../lib/valkirye-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/valkirye-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ValkiryeServerless.ValkiryeServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
